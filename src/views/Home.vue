2ddc58569b4cfc3edbc2eeaf9a0a144c30fc638b
Rica.chou <skyhigh5579@gmail.com>
2023-09-19 10:49:18 +0800

    Test husky

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
+<!--
+ * @Author: Rica.Chou
+ * @Date: 2023-09-06 11:19:37
+ * @LastEditors: Rica.Chou
+ * @LastEditTime: 2023-09-19 10:48:44
+ * @FilePath: \pqmsd:\毛\code\test\test\src\views\Home.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by Rica Chou, All Rights Reserved. 
+-->
 <script setup>
 </script>
 <template>
     <h4>
-        home
+        home2
     </h4>
 </template>
